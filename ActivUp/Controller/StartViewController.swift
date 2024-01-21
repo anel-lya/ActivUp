268c22addb979abc5ba7d4090abc9475166a7778
Anel <62986359+anel-lya@users.noreply.github.com>
2024-01-21 22:11:31 +0600

    some adds

--- ActivUp/Controller/StartViewController.swift
+++ ActivUp/Controller/StartViewController.swift
@@ -18,9 +18,15 @@ class StartViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        let isF = IsFirstDataManager.shared.fetchIsFirst()
+        if (!isF){
+            start(animated)
+        }
+        
     }
 
     @IBAction func start(_ sender: Any) {
+        
         let new = self.storyboard?.instantiateViewController(withIdentifier: "tabBarController") as? TabBarController
         new!.modalPresentationStyle = .fullScreen
         self.present(new!, animated : false, completion : nil)
